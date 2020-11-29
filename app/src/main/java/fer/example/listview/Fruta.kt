0424aaa61cc79668685818a70a61ea0e13086418
fernanda-rocha <fernanda.lennon@comunidad.unam.mx>
2020-11-29 02:42:28 -0600

    Initial commit

---
+++ app/src/main/java/fer/example/listview/Fruta.kt
@@ -0,0 +1,11 @@
+package fer.example.listview
+
+class Fruta(nombre:String, image: Int) {
+    var nombre:String = ""
+    var imagen:Int = 0
+
+    init{
+        this.nombre = nombre
+        this.imagen = imagen
+    }
+}
